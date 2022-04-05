116c6d9058e2ef37187fec663bfb2e1d038528e3
MatejKrumlovsky <matejkrumlovsky8@gmail.com>
2022-04-05 15:52:05 +0200

    domcek

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,30 +8,7 @@ public class App {
         okno.setVisible(true);
         okno.setSize(300,300);
         okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        okno.setLayout(new GridLayout(3,4));
 
-        JPanel panel = new JPanel();
-        panel.setLayout((new GridLayout(2,2)));
-
-        for (int i = 0; i < 4; i++){
-            panel.add(new JLabel("Panel " + i));
-        }
-
-        panel.setBackground(new Color(190,120,150));
-        okno.getContentPane().setBackground(Color.yellow);
-
-        JLabel label = new JLabel("Fero Hora");
-        okno.add(label);
-
-        for (int i = 0 ; i < 10; i++){
-            if (i == 5){
-                okno.add(panel);
-            }
-            else{
-                //System.out.println();
-                okno.add(new JLabel(" "+ i));
-            }
-
-        }
+        okno.add(new MyCanvas());
     }
 }
